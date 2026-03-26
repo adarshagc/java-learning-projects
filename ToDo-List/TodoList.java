7bf386a1799137840211e1092534fb04dc1537d8
adarshagc <adarshagc2004@gmail.com>
2026-03-26 23:19:44 +0530

    Changed the class file name of ToDo-List project

--- ToDo-List/TodoList.java
+++ ToDo-List/TodoList.java
@@ -3,7 +3,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main {
+public class TodoList {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ArrayList<String> tasks = new ArrayList<>();
